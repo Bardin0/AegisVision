62534accb1b74c28b41cfe37c4632028d0ecfd95
EC2 Default User <ec2-user@ip-172-31-3-205.ec2.internal>
2026-03-11 02:54:12 +0000

    Completed the scaffolding of the application for proof of concept Application is completely synced with the cloud and works through the full process, refinement of the UI and detection is required

--- src/app.py
+++ src/app.py
@@ -5,12 +5,12 @@ import uuid
 app = Flask(__name__)
 
 s3 = boto3.client("s3")
-# dynamodb = boto3.resource("dynamodb")
+dynamodb = boto3.resource("dynamodb")
 
 BUCKET_NAME = "aegis-image-bucket"
-# TABLE_NAME = "ObjectDetections"
+TABLE_NAME = "aegis-inference-results"
 
-# table = dynamodb.Table(TABLE_NAME)
+table = dynamodb.Table(TABLE_NAME)
 
 @app.route("/")
 def index():
@@ -34,17 +34,19 @@ def upload():
 
 @app.route("/results/<image_id>")
 def results(image_id):
-
     key = f"uploads/{image_id}.jpg"
+    annotated_key = f"annotated/{image_id}.jpg" 
 
-    # response = table.get_item(
-    #     Key={"image_key": key}
-    # )
+    response = table.get_item(Key={"image_key": key})
+    item = response.get("Item")
 
-    # item = response.get("Item")
+    image_url = s3.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': BUCKET_NAME, 'Key': annotated_key},
+        ExpiresIn=3600
+    )
 
-    return render_template("results.html")
-
+    return render_template("results.html", item=item, image_url=image_url)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
