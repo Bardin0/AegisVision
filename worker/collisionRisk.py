b199efe8c350b660ebf06f78522d8b440f9abf2e
mclaradn <mariaclaradnobre@gmail.com>
2026-04-01 18:55:49 -0400

    Improved math

--- worker/collisionRisk.py
+++ worker/collisionRisk.py
@@ -19,3 +19,14 @@ def isPersonDriving(person_bbox, vehicle_bbox, vehicle_label):
         vx1, vy1, vx2, vy2 = vehicle_bbox
         return not (x2 < vx1 or x1 > vx2 or y2 < vy1 or y1 > vy2)
     return False
+
+def is_overlapping(bbox1, bbox2):
+    x1, y1, x2, y2 = bbox1       # coordinates of first box
+    x1_v, y1_v, x2_v, y2_v = bbox2  # coordinates of second box
+
+    # Check if boxes do NOT overlap:
+    # If one box is completely to the left, right, above, or below the other
+    if x2 < x1_v or x1 > x2_v or y2 < y1_v or y1 > y2_v:
+        return False  # no overlap
+
+    return True  # boxes overlap
--- worker/worker.py
+++ worker/worker.py
@@ -71,10 +71,11 @@ while True:
                     continue
 
                 dist = distance(person["bbox"], vehicle["bbox"])
+                overlap = is_overlapping(person["bbox"], vehicle["bbox"])
 
-                if dist < 75:
+                if dist < 75 or overlap:
                     risk_level = "high"
-                elif dist < 170:
+                elif dist < 150:
                     risk_level = "medium"
                 else:
                     continue
